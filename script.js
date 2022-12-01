9e10c6b5c9219a20c23f71dd1f483a972af85f29
danilobezerra7 <danilobezerraedias@gmail.com>
2022-12-01 19:55:49 -0300

    add cronometro

---
+++ script.js
@@ -0,0 +1,28 @@
+var sec =0
+var min =0
+var interval
+
+function start(){
+   interval= setInterval(watch,1000)
+}
+
+function pause(){
+    clearInterval(interval)
+}
+
+function stop(){
+    clearInterval(interval)
+    sec=0
+    min=0
+    document.getElementById('watch') .innerText='00:00'
+}
+function watch(){
+    sec++
+    if(sec==60){
+       min++
+       sec=0  
+    }
+   
+    document.getElementById('watch') .innerText=min+':'+sec
+    
+}
